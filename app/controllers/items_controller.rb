7b936258e13516d328eb71daf8bce16e0f9ebc6f
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-03-12 13:48:51 +0900

    コントローラー更新

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,59 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
+  before_action :move_to_index, except: [:index, :show, :new, :create] 
+ 
+
   def index
+    #@items = Item.order("created_at DESC")
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+       redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+       redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    if@item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    end 
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :postage_id, :prefecture_id, :prepare_id, :price,:image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    if @item.order.present? || @item.user_id != current_user.id
+      redirect_to root_path
+    end 
+  end 
 end
